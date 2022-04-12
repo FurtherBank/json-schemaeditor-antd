be982c9629d92e25684dc7c8fff87eed3c45c4aa
furtherbank <1293874885@qq.com>
2022-04-12 21:06:01 +0800

    修复 react 依赖问题

--- .umirc.ts
+++ .umirc.ts
@@ -4,8 +4,8 @@ import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin';
 export default defineConfig({
   title: 'json-schemaeditor-antd',
   favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+    'https://portrait.gitee.com/uploads/avatars/user/2779/8338200_furtherbank_1645079902.png!avatar200',
+  logo: 'https://portrait.gitee.com/uploads/avatars/user/2779/8338200_furtherbank_1645079902.png!avatar200',
   outputPath: 'docs-dist',
   mode: 'site',
   // more config: https://d.umijs.org/config
